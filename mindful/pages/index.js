bde8d0b7291e34f05e00fda9855279cf06756286
KellyMenzul <kmenzul@my.bcit.ca>
2021-11-02 21:09:07 -0700

    start on landing page

--- mindful/pages/index.js
+++ mindful/pages/index.js
@@ -1,35 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import HeaderText from '../comps/HeaderText'
-import CompanionAvatar from '../comps/CompanionAvatar'
-import AboutCompanion from '../comps/AboutCompanion'
-import JokesCard from '../comps/JokeCard'
-import TalkButton from '../comps/TalkButton'
-import NavBar from '../comps/Navigation'
-import Card from '../comps/Card'
-import Buttons from '../comps/Button'
-import Breathe from '../comps/Breathe'
-import AddFriend from '../comps/AddFriend'
+import styled from 'styled-components';
+import Button from '../comps/Button';
 
+const Container = styled.div `
+height:100vh;
+width:100%;
+display:flex;
+flex-direction:row;
+align-items:center;
+background-color:red;
+`
+
+const Holder = styled.div `
+`
+
+const WelcomeHeader = styled.div `
+`
+
+const ButtonHolder = styled.div `
+`
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <AddFriend/>
-      <Breathe/>
-      <NavBar/>
-      <Card/>
-      <JokesCard/>
-      <HeaderText/>
-      <CompanionAvatar/>
-      <AboutCompanion/>
-      <TalkButton/>
-      <Buttons/>
-      <Buttons variant="outlined"/>
-      
+    <Container>
+      <Holder>
+        <WelcomeHeader>
+          <h1>Welcome to Mindful</h1>
+          <p>Invest in yourself and blah blah blah</p>
+        </WelcomeHeader>
+        <ButtonHolder>
+          
 
+        </ButtonHolder>
 
-    </div>
+      </Holder>
+      <Holder>
+
+      </Holder>
+    </Container>
   )
 }
---
+++ mindful/pages/testcomps.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import HeaderText from '../comps/HeaderText'
+import CompanionAvatar from '../comps/CompanionAvatar'
+import AboutCompanion from '../comps/AboutCompanion'
+import JokesCard from '../comps/JokeCard'
+import TalkButton from '../comps/TalkButton'
+import NavBar from '../comps/Navigation'
+import Card from '../comps/Card'
+import Buttons from '../comps/Button'
+import Breathe from '../comps/Breathe'
+import AddFriend from '../comps/AddFriend'
+
+
+export default function TestComps() {
+  return (
+    <div className={styles.container}>
+      <AddFriend/>
+      <Breathe/>
+      <NavBar/>
+      <Card/>
+      <JokesCard/>
+      <HeaderText/>
+      <CompanionAvatar/>
+      <AboutCompanion/>
+      <TalkButton/>
+      <Buttons/>
+      <Buttons variant="outlined"/>
+      
+
+
+    </div>
+  )
+}
