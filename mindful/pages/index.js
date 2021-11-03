e12cdc4ed27f91eb255d8adc1558892d4c9c8288
KellyMenzul <kmenzul@my.bcit.ca>
2021-11-02 21:41:05 -0700

    gradient and index

--- mindful/pages/index.js
+++ mindful/pages/index.js
@@ -7,34 +7,70 @@ width:100%;
 display:flex;
 flex-direction:row;
 align-items:center;
-background-color:red;
+justify-content:center;
+gap:100px;
 `
 
 const Holder = styled.div `
+display:flex;
 `
 
 const WelcomeHeader = styled.div `
 `
 
 const ButtonHolder = styled.div `
+display:flex;
+flex-direction:row;
+gap:30px;
 `
+
+const Image = styled.img `
+height:95%;
+width:100%;
+`
+const Cont1 = styled.div `
+display:flex;
+flex-direction:column;
+align-item:center;
+`
+
+const Cont2 = styled.div `
+display:flex;
+align-item:center;
+justify-content:center;
+`
+
+const Header = styled.h1 `
+font-size:70px;
+margin-bottom:-20px;
+`
+
+const Text = styled.p `
+font-size:30px;
+`
+
+
+
 
 export default function Home() {
   return (
     <Container>
       <Holder>
-        <WelcomeHeader>
-          <h1>Welcome to Mindful</h1>
-          <p>Invest in yourself and blah blah blah</p>
-        </WelcomeHeader>
-        <ButtonHolder>
-          
-
-        </ButtonHolder>
-
+        <Cont1>
+          <WelcomeHeader>
+            <Header>Welcome to Mindful</Header>
+            <Text>Invest in yourself and blah blah blah</Text>
+          </WelcomeHeader>
+          <ButtonHolder>
+            <Button ButtonText="Signup"/>
+            <Button variant="outlined" ButtonText="Login"/>
+          </ButtonHolder>
+        </Cont1>
       </Holder>
       <Holder>
-
+        <Cont2>
+          <Image src="/logo.png"/>
+        </Cont2>
       </Holder>
     </Container>
   )
