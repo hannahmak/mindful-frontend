840b31a2ce0b87e35d669c7093d32f18225c12e3
Hongsoon Kim <khsun6226@naver.com>
2021-11-10 12:08:57 -0800

    add differect paths for requests

--- mindful/next.config.js
+++ mindful/next.config.js
@@ -3,8 +3,12 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/:path*",
-        destination: "http://localhost:8080/:path*", // Proxy to Backend
+        source: "/posts",
+        destination: "http://localhost:8080/posts",
+      },
+      {
+        source: "/images/:file*",
+        destination: "http://localhost:8080/images/:file*",
       },
     ];
   },
