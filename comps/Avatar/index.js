e784a1a23147ac81cf78d4f7bd87f2149221679b
Hannah MacPherson <hannahmacpherson@Hannahs-MacBook-Pro.local>
2021-11-13 20:43:11 -0800

    changed mood bar

--- comps/Avatar/index.js
+++ comps/Avatar/index.js
@@ -2,54 +2,52 @@ import { borderRadius, width } from '@mui/system';
 import styled from 'styled-components';
 
 const Container = styled.div`
-display:flex;
-flex-direction:row;
-align-items:center;
-gap:30px;
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    gap:30px;
 `
 
-
-
 const Circle = styled.img`
-width:${props=>props.width};
-height:${props=>props.height};
-box-shadow: 0px 4px 24px -1px rgba(0, 0, 0, 0.25);
-border-radius:${props=>props.borderRadius};
-margin-top:20px;
+    width:${props=>props.width};
+    height:${props=>props.height};
+    box-shadow: 0px 4px 24px -1px rgba(0, 0, 0, 0.25);
+    border-radius:${props=>props.borderRadius};
+    margin-top:20px;
 `
 
 const Name = styled.h1`
-font-size:50px;
-color:#420062;
-display:${props=>props.show};
-margin-bottom:0px;
+    font-size:50px;
+    color:#420062;
+    display:${props=>props.show};
+    margin-bottom:0px;
 `
 
 const Holder = styled.div`
-display:flex;
-flex-direction:column;
-justify-content:center;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
 `
 
 const Follow = styled.div`
-display:${props=>props.display};
-flex-direction:row;
-gap:20px;
+    display:${props=>props.display};
+    flex-direction:row;
+    gap:20px;
 `
 const Following = styled.p`
-color:#420062;
+    color:#420062;
 `
 const FollowingNumber = styled.span`
-color:#420062;
-font-weight:800;
+    color:#420062;
+    font-weight:800;
 `
 
 const Follower = styled.p`
-color:#420062;
+    color:#420062;
 `
 const FollowerNumber = styled.span`
-color:#420062;
-font-weight:800;
+    color:#420062;
+    font-weight:800;
 `
 
 
