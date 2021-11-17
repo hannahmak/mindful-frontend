e91d8041a3a8ceaaf1171cd7555af30c0c152782
Hongsoon Kim <khsun6226@naver.com>
2021-11-16 20:07:29 -0800

    merge myprofile.js

--- pages/myprofile.js
+++ pages/myprofile.js
@@ -9,18 +9,18 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
-const Container = styled.div `
+const Container = styled.div`
 height:100vh
 width:100%;
 display:flex;
 flex-direction:row;
 `
 
-const Holder1 = styled.div `
+const Holder1 = styled.div`
 width:8%;
 `
 
-const Holder2 = styled.div `
+const Holder2 = styled.div`
 display:flex;
 flex-direction:column;
 width:70%;
@@ -29,20 +29,20 @@ height:100vh;
 gap:50px;
 `
 
-const Holder3 = styled.div `
+const Holder3 = styled.div`
 height:100vh;
 width:22%;
 `
 
-const ProfDetails = styled.div `
+const ProfDetails = styled.div`
 width:92%;
 `
 
-const MyJournal = styled.h1 `
+const MyJournal = styled.h1`
 color:#3C2743;
 `
 
-const Cont1 = styled.div `
+const Cont1 = styled.div`
 display:flex;
 flex-direction:column;
 width:80%;
@@ -51,7 +51,7 @@ gap:50px;
 padding-top:10px;
 `
 
-const JournalHolder = styled.div `
+const JournalHolder = styled.div`
 display:flex;
 flex-wrap:wrap;
 width:100%;
@@ -63,6 +63,15 @@ gap:3%;
 
 
 export default function MyProfile() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await axios.get("/posts");
+      setPosts(result.data.posts);
+    })();
+    console.log("posts", posts);
+  }, []);
   return (
     <Container>
       <Holder1>
@@ -71,7 +80,7 @@ export default function MyProfile() {
       <Holder2>
         <Cont1>
           <ProfDetails>
-            <Avatar/>
+            <Avatar />
           </ProfDetails>
           <MyJournal>My Journals</MyJournal>
           <JournalHolder>
@@ -93,7 +102,7 @@ export default function MyProfile() {
                 </CardContent>
               </CardActionArea>
               <CardActions>
-                <Button style={{borderRadius:60, color:"#2F2E4C", borderColor:"#2F2E4C"}} variant="outlined" size="small" color="primary">
+                <Button style={{ borderRadius: 60, color: "#2F2E4C", borderColor: "#2F2E4C" }} variant="outlined" size="small" color="primary">
                   Like
                 </Button>
               </CardActions>
@@ -117,20 +126,33 @@ export default function MyProfile() {
                 </CardContent>
               </CardActionArea>
               <CardActions>
-                <Button style={{borderRadius:60, color:"#2F2E4C", borderColor:"#2F2E4C"}} variant="outlined" size="small" color="primary">
+                <Button style={{ borderRadius: 60, color: "#2F2E4C", borderColor: "#2F2E4C" }} variant="outlined" size="small" color="primary">
                   Like
                 </Button>
               </CardActions>
             </Card>
+            <div>
+              <Button routeTo="./" ButtonText="Back to Home" />
+              <Button routeTo="./talk" ButtonText="Go to chat" />
+              <h1>This is Your Post</h1>
+              {posts.map((post) => (
+                <figure key={post.id}>
+                  <figcaption>{post.description}</figcaption>
+                  <figcaption>{post.timestamp}</figcaption>
+                  <img src={post.image_url}></img>
+                  <figcaption>{post.tags}</figcaption>
+                </figure>
+              ))}
+            </div>
           </JournalHolder>
 
 
-          
-          
+
+
         </Cont1>
       </Holder2>
       <Holder3>
-        <MoodBar/>
+        <MoodBar />
       </Holder3>
     </Container>
   )
