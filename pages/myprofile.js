44081d75c686c0ccbb6220e885b81bb9ceda739c
Hannah MacPherson <hannahmacpherson@Hannahs-MacBook-Pro.local>
2021-12-02 09:39:43 -0800

    fixed images

--- pages/myprofile.js
+++ pages/myprofile.js
@@ -43,7 +43,7 @@ const NewJournalButton = styled.div`
 const Holder1 = styled.div`
 display:flex;
 width:8%;
-@media only screen and (max-width: 768px) {
+@media only screen and (max-width: 1024px) {
   display:none;
 }
 `
@@ -116,13 +116,15 @@ width:100%;
 const Rowholder = styled.div `
 display:flex;
 width:100%;
-flex-direction:row;
+flex-direction:column;
 align-items:center;
 justify-content:center;
 
-@media only screen and (max-width: 900px) {
+@media only screen and (max-width: 1024px) {
   flex-direction:column;
 }
+
+
 
 
 
